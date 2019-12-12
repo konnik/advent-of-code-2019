0898447f854dcab0a4088b6b1c40913559529d68
konnik <niklas.konstenius@gmail.com>
2019-12-12 22:48:03 +0100

    Day 6 part 2

--- src/Day6.elm
+++ src/Day6.elm
@@ -1,25 +1,22 @@
 module Day6 exposing (solution)
 
 import Dict exposing (Dict)
+import Set
 import Types exposing (Solution, Solver)
-
-
-solution : Solution
-solution =
-    ( part1, part2 )
 
 
 part1 : Solver
 part1 input =
     parse input
-        |> Dict.fromList
         |> countOrbits
         |> String.fromInt
 
 
 part2 : Solver
 part2 input =
-    "not implemented"
+    parse input
+        |> countSteps
+        |> String.fromInt
 
 
 
@@ -36,6 +33,39 @@ type alias Object =
 
 
 -- count
+
+
+countSteps : Dict Object Object -> Int
+countSteps orbitMap =
+    let
+        you =
+            pathToCom "YOU" orbitMap |> Set.fromList
+
+        san =
+            pathToCom "SAN" orbitMap |> Set.fromList
+
+        steps =
+            Set.diff
+                (Set.union san you)
+                (Set.intersect san you)
+    in
+    steps
+        |> Set.size
+
+
+pathToCom : Object -> Dict Object Object -> List Object
+pathToCom from orbitMap =
+    let
+        path : Object -> List Object -> List Object
+        path curr objects =
+            case Dict.get curr orbitMap of
+                Just orbited ->
+                    path orbited (orbited :: objects)
+
+                Nothing ->
+                    objects
+    in
+    path from []
 
 
 countOrbits : Dict Object Object -> Int
@@ -59,10 +89,11 @@ countOrbits orbitMap =
 -- parse
 
 
-parse : String -> List Orbit
+parse : String -> Dict Object Object
 parse input =
     String.lines input
         |> List.map parseLine
+        |> Dict.fromList
 
 
 parseLine : String -> Orbit
@@ -73,3 +104,12 @@ parseLine line =
 
         _ ->
             ( "ERROR", "ERROR" )
+
+
+
+-- export solution
+
+
+solution : Solution
+solution =
+    ( part1, part2 )
